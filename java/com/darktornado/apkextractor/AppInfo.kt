a281ee794a174dec132b8dc410c4f77dd3f7c74e
Dark Tornado <dt3141592@naver.com>
2021-06-30 23:35:00 +0900

    .apk Extraction & Design refined

--- java/com/darktornado/apkextractor/AppInfo.kt
+++ java/com/darktornado/apkextractor/AppInfo.kt
@@ -2,6 +2,4 @@ package com.darktornado.apkextractor
 
 import android.graphics.drawable.Drawable
 
-class AppInfo(var name: String, var packageName: String, var icon: Drawable) {
-
-}
+class AppInfo(var name: String, var packageName: String, var icon: Drawable, var path: String)
--- java/com/darktornado/apkextractor/MainActivity.kt
+++ java/com/darktornado/apkextractor/MainActivity.kt
@@ -3,15 +3,30 @@ package com.darktornado.apkextractor
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.ScaleDrawable
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.Toast
 import com.darktornado.listview.Item
 import com.darktornado.listview.ListAdapter
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 class MainActivity : Activity() {
+
+    private val sdcard = Environment.getExternalStorageDirectory().absolutePath
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         permissionCheck()
@@ -29,6 +44,10 @@ class MainActivity : Activity() {
         adapter.setItems(items)
         list.adapter = adapter
         list.isFastScrollEnabled = true
+        list.onItemClickListener = OnItemClickListener { parent: AdapterView<*>?, view: View?, pos: Int, id: Long ->
+            File("$sdcard/ApkExtract/").mkdirs()
+            extractApkFile(apps.get(pos)!!)
+        }
         layout.addView(list)
         val pad = dip2px(16)
         list.setPadding(pad, pad, pad, pad)
@@ -42,10 +61,32 @@ class MainActivity : Activity() {
             val app = appList.get(n)
             val name = app.applicationInfo.loadLabel(packageManager).toString()
             val packageName = app.packageName
-            val icon = app.applicationInfo.loadIcon(packageManager)
-            apps[n] = AppInfo(name, packageName, icon)
+            val icon = resizeDrawable(app.applicationInfo.loadIcon(packageManager))
+            val path = app.applicationInfo.sourceDir
+            apps[n] = AppInfo(name, packageName, icon, path)
         }
         return apps
+    }
+
+    private fun resizeDrawable(drawable: Drawable): Drawable{
+        val bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        val canvas = Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return BitmapDrawable(Bitmap.createScaledBitmap(bitmap, dip2px(96), dip2px(96), false))
+    }
+
+    private fun extractApkFile(app: AppInfo) {
+        try {
+            val file1 = File(app.path)
+            val file2 = File("$sdcard/ApkExtract/${app.name}.apk")
+            val ch1 = FileInputStream(file1).channel
+            val ch2 = FileOutputStream(file2).channel
+            ch1.transferTo(0, ch1.size(), ch2)
+            toast(".apk file is extracted.")
+        } catch (e: Exception) {
+            toast("Extraction is Failed.\n$e")
+        }
     }
 
     private fun permissionCheck() {
