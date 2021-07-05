a06eb23136890d96ebd431d0467f68fd1bed4a15
Dark Tornado <dt3141592@naver.com>
2021-07-05 20:22:10 +0900

    App License is added.

--- java/com/darktornado/apkextractor/LicenseActivity.kt
+++ java/com/darktornado/apkextractor/LicenseActivity.kt
@@ -13,15 +13,20 @@ class LicenseActivity : Activity() {
         layout.orientation = 1
 
         val lv1 = LicenseView(this)
-        lv1.setTitle("License View")
-        lv1.setSubtitle("  by Dark Tornado, BSD 3-Clause License")
-        lv1.setLicense("BSD 3-Clause License", "LicenseView.txt")
+        lv1.setTitle("apk Extractor")
+        lv1.setSubtitle("  by Dark Tornado, MIT License")
+        lv1.setLicense("MIT License", "License.txt")
         layout.addView(lv1)
         val lv2 = LicenseView(this)
-        lv2.setTitle("Material Design")
-        lv2.setSubtitle("  by Google, Apache License 2.0")
-        lv2.setLicense("Apache License 2.0", "MaterialDesign.txt")
+        lv2.setTitle("License View")
+        lv2.setSubtitle("  by Dark Tornado, BSD 3-Clause License")
+        lv2.setLicense("BSD 3-Clause License", "LicenseView.txt")
         layout.addView(lv2)
+        val lv3 = LicenseView(this)
+        lv3.setTitle("Material Design")
+        lv3.setSubtitle("  by Google, Apache License 2.0")
+        lv3.setLicense("Apache License 2.0", "MaterialDesign.txt")
+        layout.addView(lv3)
 
         val pad = dip2px(16);
         layout.setPadding(pad, pad, pad, pad)
